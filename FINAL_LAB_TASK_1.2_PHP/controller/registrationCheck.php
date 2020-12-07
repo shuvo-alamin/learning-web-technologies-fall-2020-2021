aa5be498458699c43e806860cbf20c7425d2bee3
shuvo-alamin <72723262+shuvo-alamin@users.noreply.github.com>
2020-12-07 10:47:29 +0600

    reg check

---
+++ FINAL_LAB_TASK_1.2_PHP/controller/registrationCheck.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once('../userService.php');
+
+if(isset($_REQUEST['submit'])){
+    
+    if(!empty($_REQUEST['name']) and !empty($_REQUEST['contactNo']) and !empty($_REQUEST['username']) and !empty($_REQUEST['password'])){
+        
+        if(userRegistration($_REQUEST['name'], $_REQUEST['contactNo'], $_REQUEST['username'], $_REQUEST['password'])){
+            
+            header('location: ../view/registration.php?msg=registration_completed');
+            
+        }
+        
+        else{
+            
+            header('location: ../view/registration.php?msg=registration_failed');
+            
+        }
+        
+    }
+    
+    else{
+        
+        header('location: ../view/registration.php?msg=null');
+        
+    }
+    
+}
+
+
+
+?>
