614664f82b54bfafe467c9b92ba1721bf9095ac8
shuvo-alamin <72723262+shuvo-alamin@users.noreply.github.com>
2020-12-07 10:45:10 +0600

    login

---
+++ FINAL_LAB_TASK_1.2_PHP/controller/loginCheck.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('../userService.php');
+
+if(isset($_REQUEST['submit'])){
+    
+    if(!empty($_REQUEST['username']) and !empty($_REQUEST['password'])){
+        
+        if(validateUser($_REQUEST['username'], $_REQUEST['password'])){
+            
+            header('location: ../index.php?msg=valid');
+            
+        }
+        
+        else{
+            
+            header('location: ../index.php?msg=invalid_user');
+        }
+        
+    }
+    
+    else{
+        
+        header('location: ../index.php?msg=null');
+        
+    }
+    
+}
+
+?>
