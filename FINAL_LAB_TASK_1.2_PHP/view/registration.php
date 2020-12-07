6d2da300653a20c4bfc27dd4ad496d5f70b1a935
shuvo-alamin <72723262+shuvo-alamin@users.noreply.github.com>
2020-12-07 10:39:55 +0600

    registration

---
+++ FINAL_LAB_TASK_1.2_PHP/view/registration.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta>
+    <title>Shop - Registration</title>
+</head>
+<body>
+    
+    <br><br><br><br><br><br><br>
+    
+    <center>
+        
+        <fieldset>
+            
+            <legend><b>Registration</b></legend>
+            
+            <form action="../controller/registrationCheck.php" method="post">
+            
+                Name : <input type="text" name="name"><br><br>
+                Contact No : <input type="text" name="contactNo"><br><br>
+                UserName : <input type="text" name="username"><br><br>
+                password : <input type="password" name="password"><br><br>
+                <input type="submit" name="submit" value="Register">
+
+            </form>
+            
+        </fieldset>
+        
+        <?php
+        
+        if(isset($_REQUEST['msg'])){
+		if($_REQUEST['msg'] == 'registration_failed'){
+			echo "Registration Failed!";
+		}
+
+		if($_REQUEST['msg'] == 'null'){
+			echo "Please fill up all information!";
+		}
+        
+        if($_REQUEST['msg'] == 'registration_completed'){
+			echo "Registration Completed!";
+		}
+	
+	}
+        
+        ?>
+        
+    </center>
+    
+</body>
+</html>
