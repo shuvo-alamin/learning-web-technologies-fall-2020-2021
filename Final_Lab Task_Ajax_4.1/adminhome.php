eea6dc3bf09478c5e16444c555e23ac322379fac
shuvo-alamin <72723262+shuvo-alamin@users.noreply.github.com>
2020-12-14 10:15:18 +0600

    final commit

---
+++ Final_Lab Task_Ajax_4.1/adminhome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta>
+    <title>Admin Home</title>
+</head>
+<body>
+   
+   
+    <br><br><br><br>
+    
+    
+    <center>
+        
+        <h1>Welcome Anne</h1>
+        
+        <a href="userProfile.php">Profile</a><br>
+        <a href="changePasswordAdmin.php">Change Password</a><br>
+        <a href="viewUsers.php">View Users</a><br>
+        <a href="logout.php">Logout</a>
+        
+    </center>
+    
+</body>
+</html>
---
+++ Final_Lab Task_Ajax_4.1/userHome.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta>
+    <title>User Home</title>
+</head>
+<body>
+   
+   
+    <br><br>
+    <br><br>
+    
+    
+    <center>
+        
+        <h1>Welcome Anne</h1>
+        
+        <a href="userProfile.php">Profile</a><br>
+        <a href="changePassword.php">Change Password</a><br>
+        <a href="logout.php">Logout</a>
+        
+    </center>
+    
+</body>
+</html>
