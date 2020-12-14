97f182237a5ebef1a2a4a64becce09c6952ff624
shuvo-alamin <72723262+shuvo-alamin@users.noreply.github.com>
2020-12-14 09:18:04 +0600

    index

---
+++ Final_Lab Task_Ajax_4.1/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta>
+    <title>Login</title>
+</head>
+<body>
+    
+    <center>
+        
+        <br><br><br>
+        <br><br><br>
+        
+       
+            
+            <fieldset style="width:290px">
+            
+            <legend><h1>Login</h1></legend>
+            
+            <form>
+                
+                User Id <br>
+                <input type="text" name="userId"><br><br>
+                Password <br>
+                <input type="text" name="password"><br><br>
+                <input type="checkbox" name="rememberMe" value="Remember Me"> Remember Me
+                <hr>
+                <input type="submit" name="submit" value="Login"> <a href="registration.php">Register</a>
+                
+            </form>
+            
+        </fieldset>
+            
+            
+        
+        
+    </center>
+    
+</body>
+</html>
